3d89c2868b185d86c6959460368d95a836facd0b
kimsungsik03D <tjdtlr2620@gmail.com>
2022-03-04 18:28:27 +0900

    props를 통해 컴포넌트간 데이터 주고 받기

--- src/component/Menu.js
+++ src/component/Menu.js
@@ -1,30 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Box } from '@mui/material'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
-import { convertLength } from '@mui/material/styles/cssUtils'
 
 export default function Menu(props) {
-  const path= `/${props.link}`
-  // console.log(path);
-  // alert(path);
+  const a = props; // props에서 직접 사용하게 되면 문제가 생긴다 그렇기때문에 변수에 저장해서 사용중 왜그러자?
 
-  console.log(props.title)
   return (
     <div>
-      <Link to={path}>
-      <Box
-        sx={{
-          width: 100,
-          height: 100,
-          backgroundColor: 'red',
-        }}>
-        {props.title}
-      </Box>
+      <Link to={a.link}>
+        <Box
+          sx={{
+            width: 100,
+            height: 100,
+            backgroundColor: 'red',
+          }}>
+          {a.title}
+        </Box>
       </Link>
     </div>
   )
 }
+
 Menu.propsTypes = {
-  props: PropTypes.elementType.isRequired,
+  // props: PropTypes.elementType.isRequired,
+  props: PropTypes.node.isRequired,
 }
